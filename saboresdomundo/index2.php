af83125661cfdd9c6e3bff36a0e1733fa1593486
Higao <freitashigor@hotmail.com>
2019-09-26 20:35:03 -0300

    nova index

---
+++ saboresdomundo/index2.php
@@ -0,0 +1,24 @@
+<?php
+include 'topo.php';
+?>
+
+<!-- INCLUIR OU CRIAR AQUI SEUS ESTILOS -->
+<style>
+
+</style>
+
+<!-- CRIAR AQUI O HTML DA SUA PAGINA -->
+<div class="container">
+
+
+
+
+</div>
+<!-- FAZER AQUI A INCLUSAO DE SCRIPTS OU SEUS PROPIOS SCRIPTS -->
+<script>
+
+</script>
+
+<?php
+include 'rodape.php';
+?>
