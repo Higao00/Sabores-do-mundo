ddf7b30b9d74f3eeb6fbffdb1e77863273879dc5
Higao <freitashigor@hotmail.com>
2019-09-27 20:17:02 -0300

    lista de receita

---
+++ saboresdomundo/lista_receita.php
@@ -0,0 +1,86 @@
+<?php
+include 'topo.php';
+?>
+
+<!-- INCLUIR OU CRIAR AQUI SEUS ESTILOS -->
+<style>
+
+</style>
+
+<!-- CRIAR AQUI O HTML DA SUA PAGINA -->
+<!-- Card -->
+<div class="container">
+    <div class="card">
+        <!-- Card content -->
+        <div class="card-body ">
+            <!--Carousel Wrapper-->
+            <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+                <!--Indicators-->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+                </ol>
+                <!--/.Indicators-->
+                <!--Slides-->
+                <div class="carousel-inner" role="listbox">
+                    <!--First slide-->
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" alt="First slide">
+                    </div>
+                    <!--/First slide-->
+                    <!--Second slide-->
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg" alt="Second slide">
+                    </div>
+                    <!--/Second slide-->
+                    <!--Third slide-->
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Third slide">
+                    </div>
+                    <!--/Third slide-->
+                </div>
+                <!--/.Slides-->
+                <!--Controls-->
+                <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                <!--/.Controls-->
+            </div>
+            <br>
+            <!-- Title -->
+            <div class="row">
+                <div class="col-sm-6">
+                    <h4 class="card-title "><a>Nome da Receita</a></h4>
+                </div>
+
+                <div class="col-sm-3">
+                    <!-- Subtitle -->
+                    <h6 class="font-weight-bold indigo-text py-2"> <i class="far fa-clock"></i> 50:00 </h6>
+                </div>
+
+                <div class="col-sm-3">
+                    <!-- Icon -->
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- FAZER AQUI A INCLUSAO DE SCRIPTS OU SEUS PROPIOS SCRIPTS -->
+<script>
+
+</script>
+
+<?php
+include 'rodape.php';
+?>
